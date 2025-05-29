761fe3c3bd65dbc8f4ab6e42eda277f85be8f7c8
Vincent_Ivan <vpalomata25@gmail.com>
2025-05-29 21:48:03 +0800

    removed migrate

--- app/extensions.py
+++ app/extensions.py
@@ -2,7 +2,6 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column, Integer, String, Date, Time, Boolean
 from sqlalchemy import ForeignKey, orm, insert, select, update, and_, or_
 from sqlalchemy.exc import SQLAlchemyError,IntegrityError
-from flask_migrate import Migrate
 from flask_login import LoginManager, UserMixin
 from flask_bcrypt import Bcrypt
 import psycopg2
@@ -10,7 +9,6 @@ from psycopg2 import sql
 
 
 db = SQLAlchemy()
-migrate = Migrate()
 
 login_manager = LoginManager()
 bcrypt = Bcrypt()
